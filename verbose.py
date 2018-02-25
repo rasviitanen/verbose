b310e67b5b261072d108b3d170e09a28f4a8352e
rasviitanen <rasvi744@student.liu.se>
2018-02-26 00:41:55 +0100

    Name independent

--- verbose.py
+++ verbose.py
@@ -12,7 +12,7 @@ def verbose(func):
                     verbose_file.write(line[1])
                     verbose_file.write("    " + "print("+str(line[0])+")\n")
             imp = import_module("tmp_verbose_file_02x91923mxmsk19002177xk")
-            model = getattr(imp, "test")
+            model = getattr(imp, lines[0][1][lines[0][1].find(" ")+1:lines[0][1].find("(")])
             model(*args, **kwargs)
         finally:
             os.remove("tmp_verbose_file_02x91923mxmsk19002177xk.py")
