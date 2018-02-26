7a598c0f0e98a09d85d170fca322c5e5eabe613c
rasviitanen <rasvi744@student.liu.se>
2018-02-26 11:51:24 +0100

    Handle imports correctly

--- verbose.py
+++ verbose.py
@@ -3,10 +3,13 @@ import os
 
 from importlib import import_module
 
+
 def verbose(func):
     frame = inspect.stack()[1]
     module = inspect.getmodule(frame[0])
     file_name = module.__file__
+    this_path = os.path.dirname(os.path.abspath(__file__))
+    new_path = file_name[len(this_path)+1:-3].replace("/", ".")
     def inner_function(*args, **kwargs):
 
         lines = inspect.getsourcelines(func)
@@ -16,10 +19,13 @@ def verbose(func):
             print("|___________IGNORING DOCSTRINGS___________|")
             print("|                                         |")
             with open("tmp_verbose_file_02x91923mxmsk19002177xk.py", "w+") as verbose_file:
-                verbose_file.write("from "+ file_name[0:-3] +" import *\n")
+                verbose_file.write("from "+ new_path +" import *\n")
                 lock = False
                 for line in enumerate(lines[0][1:]):
-                    if line[1].lstrip()[0:3] == "\"\"\"" and not lock:
+                    if not line[1].lstrip():
+                        print(line[1].lstrip())
+                        continue
+                    elif line[1].lstrip()[0:3] == "\"\"\"" and not lock:
                         lock = True
                     elif line[1].lstrip()[0:3] == "\"\"\"" and lock:
                         lock = False
@@ -38,7 +44,7 @@ def verbose(func):
         finally:
             print("|_________________________________________|")
             print("|______________END OF VERBOSE_____________|")
-            os.remove("tmp_verbose_file_02x91923mxmsk19002177xk.py")
+            #os.remove("tmp_verbose_file_02x91923mxmsk19002177xk.py")
 
         return func
     return inner_function
